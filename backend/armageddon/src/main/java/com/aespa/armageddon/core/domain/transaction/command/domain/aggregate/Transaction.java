23bc1e9b52d06e221f554d77806aa89f358d84fc
hatahng <hathang16@naver.com>
2026-01-09 14:47:19 +0900

    feat(transaction): 지출/수입 엔티티 추가

---
+++ backend/armageddon/src/main/java/com/aespa/armageddon/core/domain/transaction/command/domain/aggregate/Transaction.java
@@ -0,0 +1,43 @@
+package com.aespa.armageddon.core.domain.transaction.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl_transaction")
+@NoArgsConstructor
+@Getter
+@ToString
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @Column(nullable = false)
+    private Long userNo;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(length = 255)
+    private String memo;
+
+    @Column(nullable = false)
+    private int amount;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+}
