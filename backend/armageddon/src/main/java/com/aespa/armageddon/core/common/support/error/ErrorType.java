e2e2aaba0a4836ed00cb31550ebaae451102689d
hatahng <hathang16@naver.com>
2026-01-09 14:46:25 +0900

    feat(common): 공통 ErrorType enum 정의

---
+++ backend/armageddon/src/main/java/com/aespa/armageddon/core/common/support/error/ErrorType.java
@@ -0,0 +1,45 @@
+package com.aespa.armageddon.core.common.support.error;
+
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.http.HttpStatus;
+
+public enum ErrorType {
+
+    // 기본 에러 발생
+    DEFAULT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ErrorCode.E500, "An unexpected error has occurred.", LogLevel.ERROR),
+
+    ;
+
+    private final HttpStatus status;
+
+    private final ErrorCode code;
+
+    private final String message;
+
+    private final LogLevel logLevel;
+
+    ErrorType(HttpStatus status, ErrorCode code, String message, LogLevel logLevel) {
+
+        this.status = status;
+        this.code = code;
+        this.message = message;
+        this.logLevel = logLevel;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public ErrorCode getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+}
